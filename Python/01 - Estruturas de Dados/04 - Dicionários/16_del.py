855826f3549e86ac7750dc61a2fcf28de66089fa
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-02-28 14:45:46 -0300

    Create 16_del.py

---
+++ Python/01 - Estruturas de Dados/04 - Dicionários/16_del.py
@@ -0,0 +1,12 @@
+contatos = {
+    "emerson@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+del contatos["emerson@gmail.com"]["telefone"]
+del contatos["chappie@gmail.com"]
+
+# {'emerson@gmail.com': {'nome': 'Emerson'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3443-2121'}, 'melaine@gmail.com': {'nome': 'Melaine', 'telefone': '3333-7766'}}  # noqa
+print(contatos)
