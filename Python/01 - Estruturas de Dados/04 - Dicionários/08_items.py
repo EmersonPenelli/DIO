ef3f764d64c75ac8b312d52e411f1af0d7a9f324
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-02-28 14:38:33 -0300

    Create 08_items.py

---
+++ Python/01 - Estruturas de Dados/04 - Dicionários/08_items.py
@@ -0,0 +1,4 @@
+contatos = {"emerson@gmail.com": {"nome": "Emerson", "telefone": "3333-2221"}}
+
+resultado = contatos.items()  # dict_items([('emerson@gmail.com', {'nome': 'Emerson', 'telefone': '3333-2221'})])
+print(resultado)
